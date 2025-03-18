6813604bbacf426be4b6cc47487cddfc4277509f
glg37 <bielgutjahr1@gmail.com>
2025-03-18 09:08:09 -0300

    falta apenas a fase dois e as artes

---
+++ Project Puzzle/Assets/scripts/SairDaUnity.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class SairDaUnity : MonoBehaviour
+{
+    
+    public void SairDoJogo()
+    {
+        Debug.Log("Saindo do jogo...");
+        Application.Quit(); 
+    }
+}
